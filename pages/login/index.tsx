ba3cd7d73cd39fb7a86ad1d38efd589c4345d4d9
Hanish Jindal <hanish.jindals@gmail.com>
2023-10-25 23:36:31 +0530

    fix component naming

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,7 +3,7 @@ import Login from '@/components/login/Login'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 
-const page = () => {
+const Page = () => {
     const [mobileMenu, setMobileMenu] = useState<boolean>(false)
     return (
         <div className='min-h-screen flex flex-col justify-between scrollBar overflow-y-auto lg:overflow-y-scroll select-none bg-gradient-to-b from-lighest-theme to-white relative'>
@@ -14,4 +14,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
