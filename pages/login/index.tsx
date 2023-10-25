3d7d1d788fccba95b36dc06af913b8bfb186e533
Hanish Jindal <hanish.jindals@gmail.com>
2023-10-25 23:53:21 +0530

    sigin nav fix

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,7 +6,7 @@ import Navbar from '@/components/Navbar'
 const Page = () => {
     const [mobileMenu, setMobileMenu] = useState<boolean>(false)
     return (
-        <div className='min-h-screen flex flex-col justify-between scrollBar overflow-y-auto lg:overflow-y-scroll select-none bg-gradient-to-b from-lighest-theme to-white relative'>
+        <div className='min-h-screen flex flex-col justify-between scrollBar overflow-y-auto lg:overflow-y-scroll select-none bg-gradient-to-b from-lighest-theme to-white relative' onClick={() => { setMobileMenu(false) }}>
             <Navbar mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} />
             <Login />
             <Footer />
