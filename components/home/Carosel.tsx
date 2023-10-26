d0b01be211a1ff5ffd049bc596f689a84dbd8904
Hanish Jindal <hanish.jindals@gmail.com>
2023-10-27 01:08:08 +0530

    slider carosel

--- components/home/Carosel.tsx
+++ components/home/Carosel.tsx
@@ -3,13 +3,13 @@ import { CAROSEL_DATA } from '../config';
 
 const Carosel = () => {
     return (
-        <div className='w-full h-full py-5 md:py-10 px-6 md:px-16 lg:px-28 flex flex-col items-center gap-5 lg:gap-8 relative overflow-hidden'>
+        <div className='w-full h-full py-5 md:py-10 px-6 md:px-16 lg:px-28 flex flex-col items-center gap-5 lg:gap-8 relative'>
             <h1 className='w-full text-center text-4xl lg:text-5xl font-semibold whitespace-nowrap'>
                 {CAROSEL_DATA.heading.split(' ')[0]} <span className='text-theme'>{CAROSEL_DATA.heading.split(' ')[1]}</span>
             </h1>
-            <div className='animate-scroll-small lg:animate-scroll flex w-full gap-4 lg:gap-8'>
+            <div className='flex w-full gap-4 lg:gap-8 overflow-x-auto pb-2'>
                 {CAROSEL_DATA.list.map((item, index) => (
-                    <div key={index} className='w-auto text-xs lg:text-lg border-2 border-theme rounded-2xl font-medium text-theme p-2 whitespace-nowrap scrollbar-hide'>
+                    <div key={index} className='text-xs lg:text-lg border-2 border-theme rounded-2xl font-medium text-theme p-2 whitespace-nowrap scrollbar-hide'>
                         {item}
                     </div>
                 ))}
