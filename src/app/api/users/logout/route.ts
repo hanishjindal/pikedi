f3ce2328fa79130f6c65634c6020fd5145abf5fc
Hanish Jindal <hanish.jindals@gmail.com>
2023-11-02 12:06:51 +0530

    cookie delete test

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
             message: "Logout successful",
             success: true,
         })
-        response.cookies.delete('token')
+        response.cookies.set('token', "", { expires: new Date(Date.now()) });
         return response;
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
