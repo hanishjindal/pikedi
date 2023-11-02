d502598263790aaa83bef53f0805d2e39c4bfaf7
Hanish Jindal <hanish.jindals@gmail.com>
2023-11-02 12:00:16 +0530

    delete cookie on signout

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -44,7 +44,6 @@ export async function POST(request: NextRequest) {
         })
         response.cookies.set("token", token, {
             httpOnly: true,
-
         })
         return response;
 
