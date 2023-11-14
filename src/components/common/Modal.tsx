6f29d1cfefa8d395213abf1220d9daaf853c9ed2
Hanish Jindal <hanish.jindals@gmail.com>
2023-11-15 04:09:10 +0530

    fixes in modal component

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface ModalProps {
     children: React.ReactNode;
@@ -9,12 +9,29 @@ const Modal: React.FC<ModalProps> = ({
     children,
     reset
 }) => {
+    const scrollPosition = window.scrollY;
+
+    useEffect(() => {
+
+        document.body.style.overflow = 'hidden';
+
+        // Cleanup function to enable scrolling when the modal is closed
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
+    const calculateTopMargin = () => {
+        const vhMargin = window.innerHeight * 0.3; // 30% of the viewport height
+        return `${scrollPosition + vhMargin}px`;
+    };
     return (
         <div
-            className='absolute w-screen h-screen top-0 left-0 flex justify-center items-center backdrop-blur-[2px]'
+            className='absolute w-screen h-full top-0 left-0 flex justify-center backdrop-blur-[2px] z-[10]'
             onClick={reset}
         >
-            <div className='bg-white border rounded-lg p-10 shadow-lg z-[10]'
+            <div className='bg-white border rounded-lg p-10 shadow-lg h-fit'
+                style={{ marginTop: calculateTopMargin() }}
                 onClick={e => e.stopPropagation()}
             >
                 {children}
